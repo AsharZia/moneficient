8987b3fed24c30daec14958011fb7bbb3add4890
Ashar Zia <asharzia@Ashars-MacBook-Pro.local>
2019-11-25 20:56:46 +0500

    slider added

--- lib/Registration/goal.dart
+++ lib/Registration/goal.dart
@@ -7,6 +7,7 @@ class GoalScreen extends StatefulWidget {
 }
 
 class _GoalScreenState extends State<GoalScreen> {
+  double _sliderSavingInitialValue = 1.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +20,39 @@ class _GoalScreenState extends State<GoalScreen> {
                 'Goals help you keep on track. You can add\n more goals later.'),
             buildInfoField('Title', 'Enter your title', maxLength: 20),
             buildInfoField('Price', 'Price'),
+            Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Saving amount'),
+                  trailing: Text('Rs. 10,000(20%)'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 48.0),
+                  child: Slider(
+                    value: _sliderSavingInitialValue,
+                    min: 1.0,
+                    max: 10.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        _sliderSavingInitialValue = newValue;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
             RaisedButton(
               child: Text('Set goal & continue'),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text(
+                'Add later',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                ),
+              ),
               onPressed: () {},
             ),
           ],
--- lib/Registration/register.dart
+++ lib/Registration/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moneficient/global.dart';
+import 'package:moneficient/widgets/custom_textField.dart';
 
 class RegisterScreen extends StatefulWidget {
   @override
@@ -7,11 +8,10 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-  var _sliderUtilityInitialValue = 1.0;
-  // var _sliderFoodInitialValue = 1.0;
-  // var _sliderPersonalInitialValue = 1.0;
-  // var _sliderOtherInitialValue = 1.0;
-  // final leftMarginPadding = 16.0;
+  double _sliderUtilityInitialValue = 1.0;
+  double _sliderFoodInitialValue = 1.0;
+  double _sliderPersonalInitialValue = 1.0;
+  double _sliderOtherInitialValue = 1.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,22 +25,104 @@ class _RegisterScreenState extends State<RegisterScreen> {
             const SizedBox(height: 35),
             buildInfoText(
                 'Nice to have you here. Fill the form below,\n it helps us serve you better.'),
-            buildInfoField('Name', 'Enter your name'),
+            DecoratedFormField(
+              title: 'Name',
+              placeholder: 'Enter your name',
+            ),
             buildInfoField('Salary', 'Salary'),
             buildInfoField('Savings', 'Savings'),
             buildInfoField('Currency', 'Currency'),
             Text('Income division'),
             buildInfoText(
                 'This is the amount you want to spend on each category.'),
-            buildDivisionTile(
-                sliderInitialValue: _sliderUtilityInitialValue,
-                sliderMinValue: 1.0,
-                sliderMaxValue: 10.0,
-                sliderDivisions: 10,
-                sliderLabel: '$_sliderUtilityInitialValue'),
-            // buildDivisionTile(),
-            // buildDivisionTile(),
-            // buildDivisionTile(),
+            Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Utilities'),
+                  trailing: Text('Rs. 10,000(20%)'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 48.0),
+                  child: Slider(
+                    value: _sliderUtilityInitialValue,
+                    min: 1.0,
+                    max: 10.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        _sliderUtilityInitialValue = newValue;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Food'),
+                  trailing: Text('Rs. 8,000(20%)'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 48.0),
+                  child: Slider(
+                    value: _sliderFoodInitialValue,
+                    min: 1.0,
+                    max: 10.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        _sliderFoodInitialValue = newValue;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Personal'),
+                  trailing: Text('Rs. 10,000(20%)'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 48.0),
+                  child: Slider(
+                    value: _sliderPersonalInitialValue,
+                    min: 1.0,
+                    max: 10.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        _sliderPersonalInitialValue = newValue;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Others'),
+                  trailing: Text('Rs. 10,000(20%)'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 48.0),
+                  child: Slider(
+                    value: _sliderOtherInitialValue,
+                    min: 1.0,
+                    max: 10.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        _sliderOtherInitialValue = newValue;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
             const SizedBox(height: 35),
             Center(
                 child: RaisedButton(
@@ -70,36 +152,5 @@ class _RegisterScreenState extends State<RegisterScreen> {
           decoration:
               InputDecoration(helperText: title, labelText: placeholder),
         ),
-      );
-
-  Widget buildDivisionTile(
-          {double sliderInitialValue,
-          double sliderMinValue,
-          double sliderMaxValue,
-          int sliderDivisions,
-          String sliderLabel}) =>
-      Column(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.access_alarm),
-            title: Text('Utilities'),
-            trailing: Text('Rs. 10,000(20%)'),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 48.0),
-            child: Slider(
-              value: sliderInitialValue, //_sliderUtilityInitialValue,
-              min: sliderMinValue, //1.0,
-              max: sliderMaxValue, //10.0,
-              divisions: sliderDivisions, //10,
-              label: sliderLabel, //'$_sliderUtilityInitialValue',
-              onChanged: (double newValue) {
-                setState(() {
-                  sliderInitialValue = newValue;
-                });
-              },
-            ),
-          )
-        ],
       );
 }
